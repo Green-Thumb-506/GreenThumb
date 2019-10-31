90ed1aac6d17833ea53efe25a4107a5d91e4d58e
Calvin D <cdevogel25@gmail.com>
2019-10-31 14:41:46 -0500

    added firebase and firebase config

---
+++ config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAyA2M79qFAmhUV7VGogKeKSRNI2BdjsHs",
+    authDomain: "greenthumb-de7fb.firebaseapp.com",
+    databaseURL: "https://greenthumb-de7fb.firebaseio.com",
+    projectId: "greenthumb-de7fb",
+    storageBucket: "greenthumb-de7fb.appspot.com",
+    messagingSenderId: "672806474076",
+    appId: "1:672806474076:web:246bf004413083ef943199",
+    measurementId: "G-28D2CBR20Z"
+};
+
+export default firebaseConfig;
