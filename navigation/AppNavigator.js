0c1f448325109a47a1fdcf1e827daba57fcbb605
ethLengfeld <elengfeld@wisc.edu>
2019-11-11 16:52:24 -0600

    fixing bad commit with AppNavigator

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -9,8 +9,8 @@ export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    Main: SMainAppStack,
     Auth: SAuthStack,
+    Main: SMainAppStack,
     
     
   })
