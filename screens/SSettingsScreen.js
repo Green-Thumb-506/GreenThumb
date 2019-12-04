b9b9afd456188d5d07aa2b96115dd1e2f927a412
cohej640 <32601353+cohej640@users.noreply.github.com>
2019-12-04 13:52:14 -0600

    adding the user setting functionality

--- screens/SSettingsScreen.js
+++ screens/SSettingsScreen.js
@@ -1,7 +1,24 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
 
-export default class SSettingsScreen extends React.Component {
+"use strict";
+
+import React from 'react';
+import { Text, View, StyleSheet, Alert} from 'react-native';
+import { Input, Button } from 'react-native-elements';
+import { changeEmailAuth, changePasswordAuth, deleteUserAccount } from '../services/Api.js';
+
+//class SLoginScreen extends React.PureComponent<Props, State>
+type State = {
+  email: string,
+  password: string,
+};
+
+export default class SSettingsScreen extends React.PureComponent<Props, State> {
+
+    state: State = {
+      email: '',
+      password: '',
+    };
+    
 
     static navigationOptions = {
         title: 'Settings',
@@ -9,12 +26,145 @@ export default class SSettingsScreen extends React.Component {
         headerTitleStyle: { fontSize: 40, height: 60 },
     }
 
-    render() {
+   
+
+    render(): React$Element<any> {
         return (
             <View style={styles.container}>
+                <Input
+                    placeholder='email@address.com'
+                    containerStyle={styles.inputEmail}
+                    label="Email"
+                    onChangeText={text => {
+                        /* istanbul ignore next */
+                        this._newEmail(text);
+                    }}
+                />
+                <Button
+                    title="Update Email"
+                    style={styles.button}
+                    buttonStyle={{
+                        backgroundColor:'#2c3e50',
+                    }}
+                    onPress={this._changeUserEmail}
+                />
+                <Input
+                    placeholder='new password'
+                    containerStyle={styles.inputEmail}
+                    label="Password"
+                    onChangeText={text => {
+                        /* istanbul ignore next */
+                        this._newPassword(text);
+                    }}
+                />
+                <Button
+                    title="Update Password"
+                    style={styles.button}
+                    buttonStyle={{
+                        backgroundColor:'#2c3e50',
+                    }}
+                    onPress={this._changeUserPassword}
+                />
+                <Button
+                    title="DELETE ACCOUNT!"
+                    style={styles.button}
+                    buttonStyle={{
+                        backgroundColor:'#2c3e50',
+                    }}
+                    onPress={this._deleteUserAccount}
+                />
             </View>
         );
     }
+
+
+    _newEmail = (text: string) => {
+        this.setState({
+          email: text,
+        });
+    }
+    _newPassword = (text: string) => {
+      this.setState({
+        password: text,
+      });
+  }
+
+  _deleteUserAccount = async () => {
+
+    const res = await deleteUserAccount(this.state.email);
+    //console.warn("this is the returned res, it should be null: " + res);
+    
+    if (res.error) {
+      //console.warn("I am entering the error message portion!!!")
+      Alert.alert(
+        'Error!',
+        res && res.error && res.error.message || 'There was an error changing your email!',
+        [
+          {text: 'OK', onPress: () => console.log('OK Pressed')},
+        ],
+        {cancelable: false},
+      );
+      return;
+    }
+    //console.warn("This should be navigating to the home screen!!!");
+    this.props.navigation.navigate('Settings');
+    
+  }
+
+
+    _changeUserEmail = async () => {
+        if (!this.state.email) {
+          Alert.alert(
+            'Error!',
+            'Please enter your Name, Email, and Password!',
+            [
+              {text: 'OK', onPress: () => console.log('OK Pressed')},
+            ],
+            {cancelable: false},
+          );
+          return;
+        }
+    
+        const res = await changeEmailAuth(this.state.email);
+        if (!res || res.error) {
+          Alert.alert(
+            'Error!',
+            res && res.error && res.error.message || 'There was an error changing your email!',
+            [
+              {text: 'OK', onPress: () => console.log('OK Pressed')},
+            ],
+            {cancelable: false},
+          );
+          return;
+        }
+      }
+
+      _changeUserPassword = async () => {
+        if (!this.state.password) {
+          Alert.alert(
+            'Error!',
+            'Please enter your Name, Email, and Password!',
+            [
+              {text: 'OK', onPress: () => console.log('OK Pressed')},
+            ],
+            {cancelable: false},
+          );
+          return;
+        }
+    
+        const res = await changePasswordAuth(this.state.password);
+        if (!res || res.error) {
+          Alert.alert(
+            'Error!',
+            res && res.error && res.error.message || 'There was an error changing your email!',
+            [
+              {text: 'OK', onPress: () => console.log('OK Pressed')},
+            ],
+            {cancelable: false},
+          );
+          return;
+        }
+      }
 
 }
 
--- services/Api.js
+++ services/Api.js
@@ -2,9 +2,14 @@ import Constants from 'expo-constants';
 
 /* istanbul ignore next */
 var userID  = "";
+var UserIDToken  = "";
 
 export const getUserID = () => {
   return userID;
+}
+
+export const getUserIDToken = () => {
+  return UserIDToken;
 }
 
 export const loginUser = async (userName: string, password: string): Promise<Object> => {
@@ -24,6 +29,7 @@ export const loginUser = async (userName: string, password: string): Promise<Obj
     const json = await res.json();
     /* istanbul ignore next */
     userID = json && json.localId || "";
+    UserIDToken = json && json.idToken || "";
     /* istanbul ignore next */
     email = userName;
     /* istanbul ignore next */
@@ -51,6 +57,7 @@ export const signUp = async (name: string, userName: string, password: string): 
     const json = await res.json();
     /* istanbul ignore next */
     userID = json && json.localId || "";
+    UserIDToken = json && json.idToken || "";
     addUserToRealtimeDB(name, userName);
     //API Call to Add User to Realtime DB
     return json;
@@ -149,6 +156,169 @@ export const fetchUserPlants = async (): Promise<Object> => {
     const res = await fetch(`https://greenthumb-de7fb.firebaseio.com/users/${myID}/.json`);
     const json = await res.json();
     return json && json.myGarden || null;
+  } catch (err) {
+    return null;
+  }
+}
+
+
+
+
+
+/**
+ * Here is the code that could be wonky below!!
+ * 
+ */
+// Have the users be able to update their settings!
+
+export const updateUserEmail = async (email): Promise<Object> => {
+  var data = {
+    email: email,
+  }
+  //console.warn("This will be the updated email addres: " + email);
+  const jsonData = JSON.stringify(data);
+  let request = {
+    method: "PATCH",
+    body: jsonData,
+    'Content-Type': 'application/json'
+  }
+  var userToken = getUserID();
+  //console.warn("user token" + userToken);
+  try {
+    /* istanbul ignore next */
+    const res = await fetch('https://greenthumb-de7fb.firebaseio.com/users/' + [userToken] + '.json', request);
+    const json = await res.json();
+    //console.warn("res" + JSON.stringify(json))
+    return json;
+  } catch (err) {
+    return null;
+  }
+}
+
+// This will ensure that the actual credentials are changed!!!
+export const changeEmailAuth = async (email: string): Promise<Object> => {
+  
+  let myID = getUserIDToken();
+  //console.warn("This is the ID Token for the user looking to change email: " + myID);
+  var data = {
+    idToken: myID,
+    email: email,
+    returnSecureToken: true
+  };
+  const jsonData = JSON.stringify(data);
+  let request = {
+      method: 'POST',
+      body: jsonData,
+      'Content-Type': 'application/json'
+  }
+  try {
+    /* istanbul ignore next */
+    const res = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAyA2M79qFAmhUV7VGogKeKSRNI2BdjsHs', request);
+    const json = await res.json();
+    /* istanbul ignore next */
+    //userID = json && json.localId || "";
+    //console.warn("JSON request of the change email!: " + json);
+    updateUserEmail(email);
+    //API Call to Add User to Realtime DB
+
+    return json;
+  } catch (err) {
+    return null;
+  }
+}
+
+
+/**
+ * Starting the password update section!!!!
+ * @param {} email 
+ */
+// Here is the code to update the user's password!! 
+// Have the users be able to update their settings!
+
+
+// This will ensure that the actual credentials are changed!!!
+export const changePasswordAuth = async (password: string): Promise<Object> => {
+  
+  let myID = getUserIDToken();
+  //console.warn("This is the ID Token for the user looking to change email: " + myID);
+  var data = {
+    idToken: myID,
+    password: password,
+    returnSecureToken: true
+  };
+  const jsonData = JSON.stringify(data);
+  let request = {
+      method: 'POST',
+      body: jsonData,
+      'Content-Type': 'application/json'
+  }
+  try {
+    /* istanbul ignore next */
+    const res = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAyA2M79qFAmhUV7VGogKeKSRNI2BdjsHs', request);
+    const json = await res.json();
+    /* istanbul ignore next */
+    //userID = json && json.localId || "";
+    //console.warn("JSON request for the change in password!!!: " + json);
+    //updateUserEmail(email);
+    //API Call to Add User to Realtime DB
+
+    return json;
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * THis is the code to delete the user's account!!
+ * @param {*} email 
+ */
+export const deleteUserAccount = async (email: string): Promise<Object> => {
+  
+  let myID = getUserIDToken();
+  //let uid = getUserID();
+  //console.warn("This is the ID Token for the user looking to change email: " + myID);
+  var data = {
+    idToken: myID,
+  };
+  const jsonData = JSON.stringify(data);
+  let request = {
+      method: 'POST',
+      body: jsonData,
+      'Content-Type': 'application/json'
+  }
+  try {
+    /* istanbul ignore next */
+    deleteUserAccountDB();
+    const res = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:delete?key=AIzaSyAyA2M79qFAmhUV7VGogKeKSRNI2BdjsHs', request);
+    const json = await res.json();
+    /* istanbul ignore next */
+    //userID = json && json.localId || "";
+    //console.warn("JSON request: " + json);
+    
+    //API Call to Add User to Realtime DB
+
+    return json;
+  } catch (err) {
+    return null;
+  }
+}
+
+export const deleteUserAccountDB = async (): Promise<Object> => {
+  
+  //console.warn("This will be the deleted DB entry: " + uid);
+  //const jsonData = JSON.stringify(data);
+  let request = {
+    method: "DELETE",
+    'Content-Type': 'application/json'
+  }
+  var userToken = getUserID();
+  //console.warn("user token that will be deleted!" + userToken);
+  try {
+    /* istanbul ignore next */
+    const res = await fetch('https://greenthumb-de7fb.firebaseio.com/users/' + [userToken] + '.json', request);
+    const json = await res.json();
+    //console.warn("res: " + JSON.stringify(json))
+    return json;
   } catch (err) {
     return null;
   }
